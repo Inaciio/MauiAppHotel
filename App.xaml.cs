f1963bd9e47a3f9452b6b36b199a69431562e365
DIPSOFT <inacioxavierandre@gmail.com>
2025-06-26 19:51:34 -0300

    UpDate

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,38 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> QuartoList = new List<Quarto> 
+        {
+            new Quarto()
+            {
+                descricao="Suite Super Luxo",
+                valorDiariaAdulto=100,
+                valorDiariaCrianca=55
+            },
+            new Quarto()
+            {
+                descricao="Suite  Luxo",
+                valorDiariaAdulto=85,
+                valorDiariaCrianca=40
+            },
+            new Quarto()
+            {
+                descricao="Suite  Single",
+                valorDiariaAdulto=50,
+                valorDiariaCrianca=14
+            },
+            new Quarto()
+            {
+                descricao="Suite  Crise",
+                valorDiariaAdulto=30,
+                valorDiariaCrianca=8
+            }
+
+        };
         public App()
         {
             InitializeComponent();
--- Views/ContratacaoHospedagem.xaml.cs
+++ Views/ContratacaoHospedagem.xaml.cs
@@ -2,8 +2,45 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+
+    App PropriedadesApp;
 	public ContratacaoHospedagem()
 	{
+
 		InitializeComponent();
-	}
+        PropriedadesApp= (App)Application.Current;
+        pck_quarto.ItemsSource = PropriedadesApp.QuartoList;
+
+        dtk_Checkin.MinimumDate = DateTime.Now;
+        dtk_Checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        dtk_CheckOut.MinimumDate = dtk_Checkin.Date.AddDays(1);
+        dtk_CheckOut.MaximumDate = dtk_Checkin.Date.AddMonths(6);
+
+
+    }
+
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new HospedagemContratada());
+        }
+        catch (Exception ex)
+        {
+
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+
+    }
+
+    private void dtk_Checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        
+        DatePicker elemento = sender as DatePicker;
+        DateTime dataSelecionadaCheckIn = elemento.Date;
+        dtk_CheckOut.MinimumDate = dataSelecionadaCheckIn.AddDays(1);
+        dtk_CheckOut.MaximumDate = dataSelecionadaCheckIn.AddMonths(6);
+    }
 }
