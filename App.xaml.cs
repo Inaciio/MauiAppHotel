306b903731b0e9cbb0b902fa08b2542bcde0b355
DIPSOFT <inacioxavierandre@gmail.com>
2025-06-26 11:27:55 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState? ActivationState)
+        {
+            var window = base.CreateWindow(ActivationState);    
+            window.Width = 400;  // Set the width of the window
+            window.Height = 600; // Set the height of the window
+            return window;  // Handle when your app starts
+        }
+
+    }
+}
